16d4eb6d2e7414e69cd8ac1a2dc01bbc5e475ab0
DESKTOP-QJH6G9M\joach <joachim@kelsen.nu>
2024-07-10 21:51:23 +0200

    add: initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Renci.SshNet;
+
+var arguments = new [] {"upload", "delete"};
+using var sftpClient = new SftpClient(new ConnectionInfo("localhost", 5010, "demo", new PasswordAuthenticationMethod("demo", "demo")));
+await sftpClient.ConnectAsync(CancellationToken.None);
+Console.WriteLine(sftpClient.WorkingDirectory);
+
+if (args.Length != 1 || !arguments.Contains(args[0]))
+{
+  throw new InvalidOperationException($"Expected one valid argument from {arguments}");
+}
+else if (args[0] == "upload")
+{
+  sftpClient.UploadFile(new MemoryStream("12345678"u8.ToArray()), "file.txt");
+}
+else if (args[0] == "delete")
+{
+  await sftpClient.DeleteFileAsync("file.txt", CancellationToken.None);
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # sftp-sample
-Sample console app interacting with a sftp server
+
+Console app capable of uploading a text file or deleting the text file from a SFTP server.
+The SFTP server is deployed using the docker-compose file.
+
+## Commands
+
+The following command is to upload the file
+```
+dotnet run -- upload
+```
+
+The following command is to delete the file
+```
+dotnet run -- delete
+```
